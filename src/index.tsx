7eea5bcf18eeb3912abb1170ece9d1b7cae746e9
Jinnan Zhang <vannizhang@gmail.com>
2020-02-14 10:01:06 -0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import './styles/index.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h5>hello world</h5>, 
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,125 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+
+module.exports =  (env, options)=> {
+
+    const devMode = options.mode === 'development' ? true : false;
+
+    return {
+        entry: path.resolve(__dirname, './src/index.tsx'),
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: '[name].[contenthash].js',
+            chunkFilename: '[name].[contenthash].js',
+        },
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    loader: 'ts-loader'
+                },
+                {
+                    test: /\.s?[ac]ss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader", options: {
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: 'resolve-url-loader',
+                        }, {
+                            loader: "sass-loader", options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                },
+                { test: /\.woff$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+                { test: /\.ttf$/,  loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+                { test: /\.eot$/,  loader: "file-loader" },
+                { 
+                    test: /\.svg$/,  
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000,
+                        fallback: {
+                            loader: "file-loader"
+                        }
+                    }
+                },
+                {   
+                    test: /\.(png|jpg|gif)$/,  
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000,
+                        fallback: {
+                            loader: "file-loader"
+                        }
+                    }
+                },
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: devMode ? '[name].css' : '[name].[contenthash].css',
+                chunkFilename: devMode ? '[name].css' : '[name].[contenthash].css',
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.template.html',
+                filename: 'index.html',
+                minify: {
+                    html5                          : true,
+                    collapseWhitespace             : true,
+                    minifyCSS                      : true,
+                    minifyJS                       : true,
+                    minifyURLs                     : false,
+                    removeComments                 : true,
+                    removeEmptyAttributes          : true,
+                    removeOptionalTags             : true,
+                    removeRedundantAttributes      : true,
+                    removeScriptTypeAttributes     : true,
+                    removeStyleLinkTypeAttributese : true,
+                    useShortDoctype                : true
+                }
+            })
+        ],
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    default: false,
+                    vendors: false,
+                    // vendor chunk
+                    vendor: {
+                        // sync + async chunks
+                        chunks: 'all',
+                        name: 'vendor',
+                        // import file path containing node_modules
+                        test: /node_modules/
+                    }
+                }
+            },
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: true,
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                        }
+                    }
+                }), 
+                new OptimizeCSSAssets({})
+            ]
+        },
+    }
+
+};
