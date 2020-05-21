e79e7cd9a3968273668b140804c2f8b1a40339ba
Jinnan Zhang <vannizhang@gmail.com>
2020-05-21 10:08:44 -0700

    add MapView Component

---
+++ src/components/MapView/MapView.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+
+import { loadModules, loadCss } from 'esri-loader';
+import IMapView from 'esri/views/MapView';
+import IWebMap from "esri/WebMap";
+
+interface Props {
+    webmapId: string;
+};
+
+const MapView:React.FC<Props> = ({
+    webmapId,
+    children
+})=>{
+
+    const mapDivRef = React.useRef<HTMLDivElement>();
+
+    const [ mapView, setMapView] = React.useState<IMapView>(null);
+
+    const initMapView = async()=>{
+        
+        type Modules = [typeof IMapView, typeof IWebMap];
+
+        try {
+            const [ 
+                MapView, 
+                WebMap 
+            ] = await (loadModules([
+                'esri/views/MapView',
+                'esri/WebMap',
+            ]) as Promise<Modules>);
+
+            const view = new MapView({
+                container: mapDivRef.current,
+                map: new WebMap({
+                    portalItem: {
+                        id: webmapId
+                    }  
+                }),
+            });
+
+            view.when(()=>{
+                setMapView(view);
+            });
+
+        } catch(err){   
+            console.error(err);
+        }
+    };
+
+    React.useEffect(()=>{
+        loadCss();
+        initMapView();
+    }, []);
+
+    return (
+        <>
+            <div 
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                }}
+                ref={mapDivRef}
+            ></div>
+            { 
+                React.Children.map(children, (child)=>{
+                    return React.cloneElement(child as React.ReactElement<any>, {
+                        mapView,
+                    });
+                }) 
+            }
+        </>
+    );
+};
+
+export default MapView;
