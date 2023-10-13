d4a42c0a506dad5c78f292d49352be5663d0dc94
Jinnan Zhang <vannizhang@gmail.com>
2023-10-13 10:00:27 -0700

    chore: call setDefaultOptions to use latest version of JSAPI

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,14 @@ import configureAppStore, { getPreloadedState } from './store/configureStore';
 import AppContextProvider from './contexts/AppContextProvider';
 
 import { Demo } from './pages';
+import { setDefaultOptions } from 'esri-loader';
 
 (async () => {
+    // make sure esri-loader uses latest version of the ArcGIS API for JavaScript
+    setDefaultOptions({
+        version: '4.27',
+    });
+
     const preloadedState = getPreloadedState();
 
     const root = createRoot(document.getElementById('root'));
