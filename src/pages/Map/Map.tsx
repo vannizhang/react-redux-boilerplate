7ab6353286f1ef0f2e995883db86144c20bad53e
Jinnan Zhang <vannizhang@gmail.com>
2024-03-01 15:13:23 -0800

    chore: clean up componnets move ArcGIS related components to src/pages/map

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { ErrorBoundary } from '@components/ErrorBoundary';
-import MapViewContainer from '@components/MapView/MapViewContainer';
-import WebMapSelector from '@components/WebMapSelector/WebMapSelector';
+import MapViewContainer from './MapView/MapViewContainer';
+import WebMapSelector from './WebMapSelector/WebMapSelector';
 
 export const ArcGISMapDemo = () => {
     return (
